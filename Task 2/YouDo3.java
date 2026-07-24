1e7611b1e489ccae62fdee0ba080162e09d123df
Tahia <164901668+Tahia23@users.noreply.github.com>
2026-07-25 02:06:01 +0600

    Update YouDo3.java

--- Task 2/YouDo3.java
+++ Task 2/YouDo3.java
@@ -1,1 +1,19 @@
+class Countdown {
 
+    void start(){
+
+    for(int i=5; i>=1; i--){
+        System.out.println(i);
+    }
+System.out.println("GO!");
+}
+}
+
+public class Main3{
+    public static void main(String[] args) {
+        
+        Countdown count = new Countdown();
+
+        count.start();
+    }
+}
