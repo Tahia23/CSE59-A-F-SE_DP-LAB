19c38e2df4d3554d7f3c0c44b11b8f683a651e7c
Tahia <164901668+Tahia23@users.noreply.github.com>
2026-07-27 00:37:07 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,22 @@
+class Printer {
 
+void printData(String text){
+    System.out.println("Text: " + text);
+}
+
+void printData(int number){
+    System.out.println("Number: " + number);
+}
+
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        
+        Printer p = new Printer();
+
+        p.printData("Hello!");
+
+        p.printData(200);
+    }
+}
